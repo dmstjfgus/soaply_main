909e06e20945f670e7fbdfda0646c4aa68fab0ba
dmstjfgus <shfkddl934@naver.com>
2022-11-30 12:47:59 +0900

    slider section...

--- js/custom.js
+++ js/custom.js
@@ -41,3 +41,34 @@ const sr = ScrollReveal({
 });
 
 sr.reveal(".wrapper", { dureation: 1000 });
+sr.reveal(".landing-text-box", {
+  dureation: 1000,
+  origin: "right",
+  distance: "80px",
+});
+sr.reveal(".meet-text-box", {
+  dureation: 1000,
+  origin: "bottom",
+  distance: "40px",
+});
+sr.reveal(".meet-wrapper img, .feature", {
+  dureation: 1000,
+  origin: "bottom",
+  distance: "40px",
+  interval: 200,
+});
+
+/********** Swiper Slider Effect  **********/
+const swiper = new Swiper(".swiper", {
+  loop: true, // 슬라이더 무한 반복
+  navigation: {
+    // 네비게이션
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    // 슬라이더 인디케이터
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
